3a154fcc73da8051d112a59800103f0adf0a7b70
Caio Marcelo Nardi <caio.nardi@bemfacil.com.br>
2024-11-08 15:04:40 -0300

    Criando arranjo

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,5 +1,5 @@
 import { SQSEvent, SQSHandler } from 'aws-lambda';
-import { PrismaClient } from '@prisma/client';
+import { ArranjoStatusEnum, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -10,8 +10,10 @@ export const consumeMessage: SQSHandler = async (event: SQSEvent) => {
       const body = JSON.parse(record.body);
       console.log('Processando mensagem:', body);
       let data_vencimento = new Date(body.data_vencimento);
+      let arranjo_envio = await verificaArranjo(data_vencimento, body.modalidade, body.bandeira);
       await prisma.transacao.create({
         data: {
+          arranjo_envio_id: arranjo_envio.id,
           external_id: body.external_id,
           parcela_cod: body.parcela_cod,
           nsu: body.nsu,
@@ -35,3 +37,51 @@ export const consumeMessage: SQSHandler = async (event: SQSEvent) => {
   // O Lambda de SQS não precisa retornar nada.
   return;
 };
+async function verificaArranjo(data_vencimento: Date, modalidade: string, bandeira: string) {
+  let arranjo_envio = null;
+  let arranjo = await prisma.arranjo.findUnique({
+    where: {
+      arranjo_group: {
+        data_vencimento,
+        modalidade: modalidade,
+        bandeira: bandeira
+      }
+    }
+  });
+  if (arranjo) {
+    let arranjoEnvio = await prisma.arranjoEnvio.findMany({
+      where: {
+        arranjo_id: arranjo.id,
+        status: ArranjoStatusEnum.CRIADO
+      }
+    });
+    if (arranjoEnvio.length) {
+      return arranjoEnvio[0];
+    }
+  }
+  if (!arranjo_envio) {
+    if (!arranjo) {
+      arranjo = await prisma.arranjo.create({
+        data: {
+          data_vencimento,
+          data_pagamento: data_vencimento,
+          bandeira: bandeira,
+          modalidade: modalidade,
+          create_date: new Date(),
+          update_date: new Date()
+        }
+      });
+    }
+    arranjo_envio = await prisma.arranjoEnvio.create({
+      data: {
+        arranjo_id: arranjo.id,
+        valor_enviado: null,
+        valor: 0,
+        create_date: new Date(),
+        update_date: new Date()
+      }
+    });
+  }
+  return arranjo_envio;
+}
+
